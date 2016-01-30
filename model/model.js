706fe2744b3289cf1d24171672f627d60f01f880
nighca <nighca@live.cn>
2016-01-30 12:19:03 +0800

    model write-performance optimize

--- model/model.js
+++ model/model.js
@@ -1,9 +1,21 @@
 var mongo = require('../db/mongo'),
     types = require('../db/types');
 
+var postpone = function (action) {
+    return function () {
+        var context = this;
+        var args = Array.prototype.slice.call(arguments);
+        process.nextTick(function () {
+            action.apply(context, args);
+        });
+    };
+};
+
 var createModel = function(name){
     var schema = mongo.schema({}, { strict: false }),
         model = mongo.model(name, schema);
+
+    model.emit = postpone(model.emit);
 
     ['create', 'update', 'remove'].forEach(function(op){
         model.on(op, function(){
